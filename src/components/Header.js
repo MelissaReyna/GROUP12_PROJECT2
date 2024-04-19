282bc81201f95a1809b05049c79edc94e076e609
Melissa Reyna <melissahaydee21@gmail.com>
2024-04-19 14:34:54 -0400

    Ordering components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <main className="main">
+      </main>
+    );
+  }
+}
+
+export default Header;
