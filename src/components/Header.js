fe07d295103773abeec55975732f6e0e4e3bdc34
lucasufjf <lucas.ufjf@gmail.com>
2024-04-19 14:58:42 -0400

    Adapting html to React syntax header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,43 +4,43 @@ class Header extends React.Component {
   render() {
     return (
       // < !-- ==================== HEADER ==================== -->
-      <header class="header" id="header">
-        <nav class="nav container">
-          <a href="#" class="nav_logo">
+      <header className="header" id="header">
+        <nav className="nav container">
+          <a href="#" className="nav_logo">
             <img src="assets/img/logo.png" alt="" />
           </a>
 
-          <div class="nav_menu" id="nav-menu">
-            <ul class="nav_list">
-              <li class="nav_item">
-                <a href="#home" class="nav_link">Home</a>
+          <div className="nav_menu" id="nav-menu">
+            <ul className="nav_list">
+              <li className="nav_item">
+                <a href="#home" className="nav_link">Home</a>
               </li>
-              <li class="nav_item">
-                <a href="#program" class="nav_link">Program</a>
+              <li className="nav_item">
+                <a href="#program" className="nav_link">Program</a>
               </li>
-              <li class="nav_item">
-                <a href="#choose" class="nav_link">Choose Us</a>
+              <li className="nav_item">
+                <a href="#choose" className="nav_link">Choose Us</a>
               </li>
-              <li class="nav_item">
-                <a href="#pricing" class="nav_link">Pricing</a>
+              <li className="nav_item">
+                <a href="#pricing" className="nav_link">Pricing</a>
               </li>
 
-              <div class="nav_link">
-                <a href="#footer" class="button nav_button">
+              <div className="nav_link">
+                <a href="#footer" className="button nav_button">
                   Register Now
                 </a>
               </div>
             </ul>
 
             {/* <!-- Close button --> */}
-            <div class="nav_close" id="nav-close">
-              <i class="ri-close-line"></i>
+            <div className="nav_close" id="nav-close">
+              <i className="ri-close-line"></i>
             </div>
           </div>
 
           {/* <!-- Toggle button --> */}
-          <div class="nav_toggle" id="nav-toggle">
-            <i class="ri-menu-3-line"></i>
+          <div className="nav_toggle" id="nav-toggle">
+            <i className="ri-menu-3-line"></i>
           </div>
         </nav>
       </header>
