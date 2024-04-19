1d4ca1d3f5a8789d511e0c168d9c158c0d4dcf65
lucasufjf <lucas.ufjf@gmail.com>
2024-04-19 14:59:19 -0400

    Adapting html to React syntax footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,97 +4,97 @@ class Footer extends React.Component {
   render() {
     return (
       // < !-- ==================== FOOTER ==================== -->
-      <footer class="footer section" id="footer">
-        <div class="footer_container container grid">
+      <footer className="footer section" id="footer">
+        <div className="footer_container container grid">
           <div>
-            <a href="#" class="footer_logo">
+            <a href="#" className="footer_logo">
               <img src="assets/img/logo.png" alt=""/>
             </a>
-            <p class="footer_description">
+            <p className="footer_description">
               Subscribe for company updates below.
             </p>
 
-            <form action="" class="footer_form" id="contact-form">
-              <input type="email" name="user_email" placeholder="Your Email" class="footer_input"
+            <form action="" className="footer_form" id="contact-form">
+              <input type="email" name="user_email" placeholder="Your Email" className="footer_input"
                 id="contact-user"/>
-                <button type="submit" class="button">
+                <button type="submit" className="button">
                   Subscribe
                 </button>
             </form>
 
-            <p class="footer_message" id="contact-message"></p>
+            <p className="footer_message" id="contact-message"></p>
           </div>
 
-          <div class="footer_content">
+          <div className="footer_content">
             <div>
-              <h3 class="footer_title">Services</h3>
+              <h3 className="footer_title">Services</h3>
 
-              <ul class="footer_links">
+              <ul className="footer_links">
                 <li>
-                  <a href="#" class="footer_link">Flex Muscle</a>
+                  <a href="#" className="footer_link">Flex Muscle</a>
                 </li>
                 <li>
-                  <a href="#" class="footer_link">Cardio Exercises</a>
+                  <a href="#" className="footer_link">Cardio Exercises</a>
                 </li>
                 <li>
-                  <a href="#" class="footer_link">Basic Yoga</a>
+                  <a href="#" className="footer_link">Basic Yoga</a>
                 </li>
                 <li>
-                  <a href="#" class="footer_link">Weight Lifting</a>
+                  <a href="#" className="footer_link">Weight Lifting</a>
                 </li>
               </ul>
             </div>
             <div>
-              <h3 class="footer_title">Pricing</h3>
+              <h3 className="footer_title">Pricing</h3>
 
-              <ul class="footer_links">
+              <ul className="footer_links">
                 <li>
-                  <a href="#" class="footer_link">Basic</a>
+                  <a href="#" className="footer_link">Basic</a>
                 </li>
                 <li>
-                  <a href="#" class="footer_link">Standard</a>
+                  <a href="#" className="footer_link">Standard</a>
                 </li>
                 <li>
-                  <a href="#" class="footer_link">Diamond</a>
+                  <a href="#" className="footer_link">Diamond</a>
                 </li>
               </ul>
             </div>
             <div>
-              <h3 class="footer_title">COMPANY</h3>
+              <h3 className="footer_title">COMPANY</h3>
 
-              <ul class="footer_links">
+              <ul className="footer_links">
                 <li>
-                  <a href="#" class="footer_link">About Us</a>
+                  <a href="#" className="footer_link">About Us</a>
                 </li>
                 <li>
-                  <a href="#" class="footer_link">Careers</a>
+                  <a href="#" className="footer_link">Careers</a>
                 </li>
                 <li>
-                  <a href="#" class="footer_link">Customers</a>
+                  <a href="#" className="footer_link">Customers</a>
                 </li>
                 <li>
-                  <a href="#" class="footer_link">Partners</a>
+                  <a href="#" className="footer_link">Partners</a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
 
-        <div class="container">
-          <div class="footer_group">
-            <ul class="footer_social">
-              <a href="#" class="footer_social-link">
-                <i class="ri-facebook-circle-fill"></i>
+        <div className="container">
+          <div className="footer_group">
+            <ul className="footer_social">
+              <a href="#" className="footer_social-link">
+                <i className="ri-facebook-circle-fill"></i>
               </a>
-              <a href="#" class="footer_social-link">
-                <i class="ri-instagram-line"></i>
+              <a href="#" className="footer_social-link">
+                <i className="ri-instagram-line"></i>
               </a>
-              <a href="#" class="footer_social-link">
-                <i class="ri-youtube-line"></i>
+              <a href="#" className="footer_social-link">
+                <i className="ri-youtube-line"></i>
               </a>
             </ul>
 
-            <span class="footer_copy">
+            <span className="footer_copy">
               2024 &#169; Copyright B-Fit. All rights reserved.
             </span>
           </div>
