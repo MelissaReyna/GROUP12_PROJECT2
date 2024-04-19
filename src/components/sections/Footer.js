1018bc17c9f8561a97257d4134563e19d71501d3
lucasufjf <lucas.ufjf@gmail.com>
2024-04-19 14:29:54 -0400

    Creating structure Footer component

---
+++ src/components/sections/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <main className="main">
+      </main>
+    );
+  }
+}
+
+export default Footer;
