531d294f641e206c3eac42976bbf462ba0900d8c
lucasufjf <lucas.ufjf@gmail.com>
2024-04-19 16:51:56 -0400

    Adding Programs Data component

---
+++ src/components/ProgramsData.js
@@ -0,0 +1,33 @@
+import program1 from '../img/program1.png'
+import program2 from '../img/program2.png'
+import program3 from '../img/program3.png'
+import program4 from '../img/program4.png'
+
+const programs = [
+    {
+        id: 1,
+        title: "Weight Loss Challenge",
+        description: "A structured program aimed at helping participants lose weight through a combination of cardiovascular exercise, strength training, and nutrition guidance.",
+        imgSrc: program1
+    },
+    {
+        id: 2,
+        title: "Muscle Building Program",
+        description: "A workout plan designed to increase muscle mass and strength, incorporating resistance training exercises targeting major muscle groups.",
+        imgSrc: program2
+    },
+    {
+        id: 3,
+        title: "Yoga and Meditation Series",
+        description: "A series of yoga classes and guided meditation sessions focused on stress reduction, relaxation, and improving overall well-being.",
+        imgSrc: program3
+    },
+    {
+        id: 4,
+        title: "Specialized Training Programs",
+        description: "Programs tailored to specific fitness goals or interests, such as marathon training, obstacle course preparation, or sports-specific conditioning.",
+        imgSrc: program4
+    }
+];
+
+export default programs;
