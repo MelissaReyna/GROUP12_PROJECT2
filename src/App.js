d58b716fc4f4c8aa54b6435b0f4e2a3343d4a405
Melissa Reyna <melissahaydee21@gmail.com>
2024-04-19 17:16:38 -0400

    Updating App Links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Program from "./components/sections/Program";
+import Choose from "./components/sections/Choose";
+import Pricing from "./components/sections/Pricing";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Home />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/program' component={Program} />
+        <Route path='/choose' component={Choose} />
+        <Route path='/pricing' component={Pricing} />
+      </Switch>
       <Footer />
     </Router>
   );
 }
 
 export default App;
-
